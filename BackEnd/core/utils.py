ca62b878008afdd310995f04f351f8911da90c0c
manuelamaroni <manuela.maroni@mail.polimi.it>
2024-12-15 13:04:05 +0100

    prompt fix

--- BackEnd/core/utils.py
+++ BackEnd/core/utils.py
@@ -282,7 +282,45 @@ def generate_itinerary(lat, lng, start_date, end_date, start_hour, end_hour, num
     model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     generation_config=generation_config,
-    system_instruction="Scenario: A user wants to visit a city for a specific number of days. \n\nYour Goal: Generate 3 distinct itineraries, each tailored to the user's preferences and requirements.\n\nYou will receive an object with the following fields:\nInput Fields:\n1. City: Latitude and longitude (lat, lng) of the city.\n2. Arrival Date: Arrival date and hour.\n3. Departure Date: Departure date and hour.\n4. Number of Days: The number of days the trip should last, taking into consideration the given dates.\n5. Number of Travelers: Seniors (65+), Adults (24–65), Youth (13–24), Children (<13).\n6. Budget: User's preference (Cheap, Balanced, Luxury or Flexible).\n7. Places: A JSON of all available attractions in the city , where each place includes:\n\ta. Name: name of the place (you should refer to a place using this field when returning the selected attractions).\n\tb. Associated Product: Details about a purchasable product related to the attraction, such as tours, museum visits, or activities (Not all the attractions will have an associated product).\n\tc. Summary: Information about the product.\n\td. Price of the product.\n7. Required attractions: attractions that must be included.\n8. Removed attractions: attractions that must be excluded.\n9. Categories: the categories the user has selected as preferences\n\nRequirements:\n1. The itineraries must cover all available days from arrival to departure. (example: if a user arrives 2024-10-22 12:00:00 and departures 2024-10-25 18:00:00 the first visit must start at 2024-10-22 12:00:00 and the last visit must end at 2024-10-25 18:00:00. All the other visits must be included between start date and end date).\n2. Ensure to use all the time the user has to visit the city, also the day of the departure if possible.\n3. All itineraries must Include attractions in required_places and exclude attractions in removed_places.\n4. Select additional attractions based on reviews, ratings, and user preferences.\n5. Focus on different themes for each itinerary (e.g., cultural, historical, adventure, entertainment). Select based on the age group of the traveller.\n6. Schedule at least 1 or 2 hour of spare time between consecutive visits, that is time for travel and breaks.\n7. If you select a place, the visit will be based on the product associated, so be aware of what the product includes and take into consideration the product summary and information.\n\nFor each itinerary you must retrieve these informations: \n1. itineraryName: Assign a unique and meaningful name to each itinerary that reflects its theme, focus, or style (e.g., 'Cultural Escapade', 'Adventure Highlights', 'Relaxed Retreat'). Avoid generic names such as 'Itinerary 1' or 'Itinerary 2'.\n2. list of attractions with: \n\ta. Name: the name of the attraction must be equal to Attraction Name, you can’t change it. (Example: If you receive a list with a place: 'Duomo di Milano’ and you want to use it, in the name you must put 'Duomo di Milano', without any changes).\n\tb. Starting Hour and EndingHour: Specify the starting and ending times for each attraction visit in the format HH:MM. Consider an average range of duration for each attractions (example: if it’s a museum minimum 3 hours, if it’s a dinnerShow minimum 3-4 hours). Consider also that some attractions must be done at specific range of time (example: a dinner should start between 19 and 23).\n\tc. Day: the day in which the visit is scheduled "
+    system_instruction="""
+        Scenario: A user wants to visit a city for a specific number of days. 
+
+        Your Goal: Generate 3 distinct itineraries, each tailored to the user's preferences and requirements.
+
+        You will receive an object with the following fields:
+        Input Fields:
+        1. City: Latitude and longitude (lat, lng) of the city.
+        2. Start Date: Arrival date and hour.
+        3. End Date: Departure date and hour.
+        4. Number of Travelers: Seniors (65+), Adults (24-65), Youth (13-24), Children (<13).
+        5. Budget: User's preference (Cheap, Balanced, Luxury or Flexible).
+        6. Places: A list of all available attractions in the city, where each place includes:
+            a. Attraction Name: name of the place.
+                Some places will also include:
+            b. Associated Product: Details about a purchasable product related to the attraction, such as tours, museum visits, or activities.
+            c. What's included: List of items included in the product.
+            d. Price of the product.
+        7. Required attractions: attractions that must be included.
+        8. Removed attractions: attractions that must be excluded.
+        9. Categories: the categories the user has selected, the chosen attraction must be of those categories
+
+        Requirements:
+        1. The itineraries must cover all available days from arrival to departure. (example: if a user arrives 2024-10-22 12:00:00 and departures 2024-10-25 18:00:00 the first visit must start at 2024-10-22 12:00:00 and the last visit must end at 2024-10-25 18:00:00. All the other visits must be included between start date and end date). Do not schedule visits after the departure date and hour.
+        2. Ensure to use all the time the user has to visit the city, also the day of the departure if possible.
+        3. All itineraries must Include attractions in required_places and exclude attractions in removed_places. The attractions in required_places do do not have to be necessarily at the beginning of the itinerary, but can be also put in the middle or at the end of the itineray.
+        4. Select additional attractions based on reviews, ratings, and user preferences.
+        5. Focus on different themes for each itinerary (e.g., cultural, historical, adventure, entertainment). Select based on the age group of the traveller.
+        6. Always schedule at least 1 or 2 hour of spare time between consecutive visits, that is time for travel and breaks.
+        7. If you select a place, the visit will be based on the product associated, so be aware of what the product includes and take into consideration the product summary and information.
+        8. The attractions must be selected based on the categories the user chosen (e.g., if the user has selected the category 'Stadium', each itinerary must contain at least one attraction belonging to the 'Stadium' Category). There must be at least one attraction per category in all itineraries. The categories of the attractions should be inferred from their name and summary.
+
+        For each itinerary you must retrieve these informations: 
+        1. itineraryName: Assign a unique and meaningful name to each itinerary that reflects its theme, focus, or style (e.g., 'Cultural Escapade', 'Adventure Highlights', 'Relaxed Retreat'). Avoid generic names such as 'Itinerary 1' or 'Itinerary 2'.
+        2. list of attractions with: 
+            a. Name: the name of the attraction must be equal to Attraction Name, you can’t change it. (Example: If you receive a list with a place: 'Duomo di Milano’ and you want to use it, in the name you must put 'Duomo di Milano', without any changes).
+            b. Starting Hour and EndingHour: Specify the starting and ending times for each attraction visit in the format HH:MM. Consider an average range of duration for each attractions (example: if it's a museum minimum 3 hours, if it's a dinnerShow minimum 3-4 hours). Consider also that some attractions must be done at specific range of time (example: a dinner should start between 19 and 23). Remember that at least 1 or 2 hours of spare time should be scheduled between two consecutive visits. Do not schedule visits after the departure hour of the user.
+            c. Day: the day in which the visit is scheduled
+    """
     )
 
     chat_session = model.start_chat(
